ecd3e3d28f4ee766cf750ee0f8461bfe3cbe3e3b
Erik Ayala <ayala.erik61@gmail.com>
2021-07-29 13:57:44 -0500

    alias exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,21 @@
+use employees;
+
+select *
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya') order by first_name;
+
+select *
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya') order by first_name, last_name;
+
+select *
+from employees.employees
+where first_name in ('Irena', 'Vidya', 'Maya') order by last_name, first_name;
+
+select *
+from employees
+where last_name like 'E%' order by emp_no;
+
+select *
+from employees
+where last_name like 'E%' order by emp_no desc ;
